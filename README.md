9ea618a02a53a2e6b235523958e1c95117a8c2d5
kelli <kellikells@gmail.com>
2021-07-21 00:19:13 -0700

    tests/1 done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # [Quality Assurance with Chai](https://www.freecodecamp.org/learn/quality-assurance/quality-assurance-and-testing-with-chai/)
+
+
+# A headless browser:
+ is a web browser WITHOUT a graphical user interface. This kind of tool is particularly useful for testing web pages, as it is able to render and understand HTML, CSS, and JavaScript the same way a browser would.
+
+# Zombie.JS
+It's a lightweight browser which is totally based on JS, without relying on additional binaries to be installed. This feature makes it usable in an environment such as Replit. 
+
+# Mocha 
+Allows you to prepare the ground running some code before the actual tests. 
+
+With a headless browser, before the actual testing, we need to visit the page we are going to inspect. The suiteSetup 'hook' is executed only once at the suite startup. Other different hook types can be executed before each test, after each test, or at the end of a suite. See the Mocha docs for more information.
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,8 +14,8 @@ suite("Functional Tests", function () {
         .request(server)
         .get("/hello")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello Guest");
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello Guest");
           done();
         });
     });
@@ -25,8 +25,8 @@ suite("Functional Tests", function () {
         .request(server)
         .get("/hello?name=xy_z")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello xy_z");
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello xy_z");
           done();
         });
     });
@@ -35,40 +35,51 @@ suite("Functional Tests", function () {
       chai
         .request(server)
         .put("/travellers")
+        .send({ surname: 'Colombo' })
 
         .end(function (err, res) {
-          assert.fail();
+          assert.equal(res.status, 200, 'response should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.equal(res.body.name, 'Cristoforo', 'res.body.name should be Cristoforo');
+          assert.equal(res.body.surname, 'Colombo', 'res.body.surname should be Colombo');
 
           done();
         });
     });
     // #4
     test('send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai
+        .request(server)
+        .put("/travellers")
+        .send({ surname: 'Verrazzano' })
+        .end(function (err, res) {
+          assert.equal(res.status, 200, 'response should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.equal(res.body.name, 'Giovanni', 'res.body.name should be Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano', 'res.body.surname should be de Verrazzano');
+          done();
+        });
     });
   });
-});
 
-const Browser = require("zombie");
+  // const Browser = require("zombie");
 
-suite("Functional Tests with Zombie.js", function () {
+  // suite("Functional Tests with Zombie.js", function () {
 
-  suite('"Famous Italian Explorers" form', function () {
-    // #5
-    test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
-      browser.fill("surname", "Colombo").pressButton("submit", function () {
-        assert.fail();
+  //   suite('"Famous Italian Explorers" form', function () {
+  //     // #5
+  //     test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
+  //       browser.fill("surname", "Colombo").pressButton("submit", function () {
+  //         assert.fail();
 
-        done();
-      });
-    });
-    // #6
-    test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-      assert.fail();
+  //         done();
+  //       });
+  //     });
+  //     // #6
+  //     test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
+  //       assert.fail();
 
-      done();
-    });
-  });
+  //       done();
+  //     });
+  //   });
 });
