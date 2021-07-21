e896fdf24c08157c252f97c1223d75662ce29887
kelli <kellikells@gmail.com>
2021-07-21 00:54:45 -0700

    tests/2 done

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,7 +8,7 @@ chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
   suite("Integration tests with chai-http", function () {
-    // #1
+    // #1 - 19
     test("Test GET /hello with no name", function (done) {
       chai
         .request(server)
@@ -19,7 +19,7 @@ suite("Functional Tests", function () {
           done();
         });
     });
-    // #2
+    // #2 -20
     test("Test GET /hello with your name", function (done) {
       chai
         .request(server)
@@ -30,7 +30,7 @@ suite("Functional Tests", function () {
           done();
         });
     });
-    // #3
+    // #3 -21
     test('send {surname: "Colombo"}', function (done) {
       chai
         .request(server)
@@ -46,7 +46,7 @@ suite("Functional Tests", function () {
           done();
         });
     });
-    // #4
+    // #4 -22
     test('send {surname: "da Verrazzano"}', function (done) {
       chai
         .request(server)
@@ -62,24 +62,48 @@ suite("Functional Tests", function () {
     });
   });
 
-  // const Browser = require("zombie");
+  const Browser = require("zombie");
+  Browser.site = 'https://boilerplate-mochachai-1.kellikells.repl.co'; // Your URL here
 
-  // suite("Functional Tests with Zombie.js", function () {
 
-  //   suite('"Famous Italian Explorers" form', function () {
-  //     // #5
-  //     test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
-  //       browser.fill("surname", "Colombo").pressButton("submit", function () {
-  //         assert.fail();
+  suite("Functional Tests with Zombie.js", function () {
+    const browser = new Browser();
+    suiteSetup(function (done) {
+      return browser.visit('/', done);
+    });
+    suite('"Famous Italian Explorers" form', function () {
+      // #5 -23
+      test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
+        browser.fill("surname", "Colombo").pressButton("submit", function () {
+          browser.assert.success(browser.status, 200);
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
 
-  //         done();
-  //       });
-  //     });
-  //     // #6
-  //     test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-  //       assert.fail();
+          done();
+        });
+      });
+      // #6 -24
+      test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
+        browser.fill('surname', 'Vespucci').pressButton('submit', function () {
+          browser.assert.success(browser.status, 200);
+          browser.assert.text('span#name', 'Amerigo');
+          broswer.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+          //   })
+          // Fill in the form with the surname of Vespucci
+          // Submit it pressing 'submit' button
+          // Within the callback:
 
-  //       done();
-  //     });
-  //   });
-});
+          // assert that status is 200
+          // assert that the text inside the element span#name is 'Amerigo'
+          // assert that the text inside the element span#surname is 'Vespucci'
+          // assert that the element(s) span#dates exist and their count is 1
+
+
+          done();
+        });
+      });
+    })
+  })
+  });
